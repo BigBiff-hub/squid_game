258a2fcbb5fa0dac70a190bb70c5890764a5dfd9
BigBiff-hub <77340730+BigBiff-hub@users.noreply.github.com>
2021-11-11 23:07:23 +0000

    javascript to get 3d model of the doll

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// const geometry = new THREE.BoxGeometry();
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
+const light = new THREE.AmbientLight(0xffffff); // soft white light
+scene.add(light);
+
+camera.position.z = 5;
+// Instantiate a loader
+const loader = new THREE.GLTFLoader();
+loader.load("../squid_model/scene.gltf", function (gltf) {
+    scene.add(gltf.scene);
+    gltf.scene.scale.set(.5, .5, .5);
+})
+
+function animate() {
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+}
+animate();
+
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight)
+}
