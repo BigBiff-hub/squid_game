c0300e6953c40af902f34cec7548d772b1216d1c
BigBiff-hub <77340730+BigBiff-hub@users.noreply.github.com>
2021-11-17 19:15:42 +0000

    add player class

--- js/main.js
+++ js/main.js
@@ -59,7 +59,16 @@ function createCourse() {
 }
 createCourse()
 
+class player_1 {
+    constructor() {
+        const geometry = new THREE.SphereGeometry(15, 32, 16);
+        const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+        const sphere = new THREE.Mesh(geometry, material);
+        scene.add(sphere);
+    }
 
+}
+const player = new player_1()
 setTimeout(() => {
     doll.lookBackward()
 }, 1500);
